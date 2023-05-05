e14fe588294588498664e6126243ede3788b8f3a
Yshen <yishengwei@pinming.cn>
2023-05-05 16:55:37 +0800

    feat: 类型文件需要打包

--- packages/tool/index.ts
+++ packages/tool/index.ts
@@ -2,11 +2,10 @@
  * @Author: Yshen yishengwei@pinming.cn
  * @Date: 2023-05-04 09:59:34
  * @LastEditors: Yshen yishengwei@pinming.cn
- * @LastEditTime: 2023-05-05 14:58:58
+ * @LastEditTime: 2023-05-05 16:27:10
  * @FilePath: /uni-app-components/packages/tool/index.ts
  * @Description: app.use(Tool)
  */
-import Vue from 'vue'
 import * as ob from './lib/ob'
 
 declare global {
@@ -15,9 +14,8 @@ declare global {
     }
 }
 
-const install = (app, options) => {
-    console.log('options', options)
-    app.config.globalProperties.ob = ob;
+const install = (Vue) => {
+    Vue.config.globalProperties.ob = ob;
     window.ob = ob;
     // 将ob对象挂载到全局对象上
     // 定义全局 mixin，将 uni 注册到应用上下文中
